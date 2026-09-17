939a16de941449da23ccae0f0d67f874b7232792
Josh-code06 <joshua.reyesad0604@gmail.com>
2026-09-17 18:47:11 -0500

    Add files via upload

---
+++ Ejemplo5.php
@@ -0,0 +1,12 @@
+<html>
+<head><title> EJEMPLO DE VARIABLES </title></head>
+<body>
+    <h1>Ejemplo de funciones para las variables</h1>
+    <?php
+    $ed = 42;
+    $nom = 'Juan';
+    if (is_integer ($ed) && is_string ($nom))
+        print ("$nom tiene $ed años");
+    ?>  
+</body>
+</html>
